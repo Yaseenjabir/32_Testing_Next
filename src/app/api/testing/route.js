555aa518aa053b3a4092c7a988ca40b5b4124871
Yaseenjabir <yaseenkhan1235476@gmail.com>
2024-09-20 18:46:13 +0500

    init

--- src/app/api/testing/route.js
+++ src/app/api/testing/route.js
@@ -1,7 +1,16 @@
+import mongoose from "mongoose";
 import { NextResponse } from "next/server";
+import { dbref } from "../../../../db/database";
+import { testingSchema } from "../../../../Schema/TestingSchema";
 
 export async function POST(req) {
-  const data = await req.json();
+  const payload = await req.json();
 
-  return NextResponse.json(data);
+  await mongoose.connect(dbref);
+
+  const data = new testingSchema(payload);
+
+  const result = await data.save();
+
+  return NextResponse.json(result);
 }
